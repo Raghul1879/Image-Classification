344e9c2047869dd68dfbedb32fb9b7be6ee9b4b6
Raghul <99247919+Raghul1879@users.noreply.github.com>
2023-06-19 16:43:52 +0530

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,30 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Load the MNIST dataset (or any other dataset of your choice)
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scale the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Create an instance of the K-Nearest Neighbors classifier
+k = 5  # Number of neighbors
+classifier = KNeighborsClassifier(n_neighbors=k)
+
+# Train the classifier
+classifier.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = classifier.predict(X_test)
+
+# Calculate the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
